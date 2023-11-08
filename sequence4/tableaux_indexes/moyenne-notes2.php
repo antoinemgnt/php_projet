b9eef454664618313aa33188f6efb174ad0468c3
Mignota <antoinemignot25@gmail.com>
2023-11-08 11:50:51 +0100

    moyenne-notes2.php terminer

--- sequence4/tableaux_indexes/moyenne-notes2.php
+++ sequence4/tableaux_indexes/moyenne-notes2.php
@@ -7,4 +7,15 @@ $sommeNote = array_sum($notes);
 $i= count($notes);
 
 $moyenne = $sommeNote / $i;
-echo "La moyenne de classe à l'examen final est de: $sommeNote / $i = $moyenne";
+echo "La moyenne de cet étudiant à l'examen final est de: $sommeNote / $i = $moyenne";
+if ($moyenne < 10) {
+    echo "Vous n'avez pas votre diplôme";
+} elseif ($moyenne >= 10 && $moyenne < 12){
+    echo " Mention passable";
+} elseif ($moyenne >= 12 && $moyenne < 14){
+    echo " Mention assez bien";
+} elseif ($moyenne >= 14 && $moyenne < 16){
+    echo " Mention bien";
+} else {
+    echo " Mention très bien";
+}
