7ebac59ed05b5b4431765e39c5f60faac60f38ee
Mignota <antoinemignot25@gmail.com>
2023-11-08 11:55:43 +0100

    moyenne-notes2.php terminer

---
+++ sequence4/tableaux_indexes/recherhe-equipe.php
@@ -0,0 +1,6 @@
+<?php
+$utilisateur = readline("Saisir le nom d'une équipe : ");
+$utilisateur = strtolower($utilisateur);
+$utilisateur = ucfirst($utilisateur);
+$equipe = ['France','Brésil','Argentine','Espagne','Sénégal','Australie','Belgique'];
+
