55324ec65e6335665afc402f53ba1f324ad89763
Mignota <antoinemignot25@gmail.com>
2023-11-08 11:42:57 +0100

    moyenne-notes.php terminer

---
+++ sequence4/tableaux_indexes/moyenne-notes.php
@@ -0,0 +1,11 @@
+<?php
+$notes = [10,12.5,14,7,15,9.5,8];
+foreach ($notes as $note) {
+    echo $note . PHP_EOL;
+}
+$sommeNote = array_sum($notes);
+$i= count($notes);
+
+$moyenne = $sommeNote / $i;
+echo "La moyenne de classe à l'examen final est de: $sommeNote / $i = $moyenne";
+
